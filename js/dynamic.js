eceb8302e31df1ff0d27e32ac97cafdd33ecbd2c
kano <kanokanombola@gmail.com>
2022-02-01 11:10:23 +0200

    Modify dynamic.js

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,21 +1,26 @@
-const jtonav = document.getElementsByClassName('tonav')[0];
-const menulist = document.querySelectorAll('.menu > li');
-const togmenu = document.getElementsByClassName('menu')[0];
+const humburgerNav = document.getElementsByClassName("tonav")[0];
+const menulist = document.querySelectorAll(".menu > li");
+const togmenu = document.getElementsByClassName("menu")[0];
+togmenu.style.display = "none";
 
 menulist.forEach((element) => {
-  element.addEventListener('click', () => { togmenu.style.display = 'none'; });
+  element.addEventListener("click", () => {
+    togmenu.style.display = "none";
+    humburgerNav.style.color = "#6070ff";
+    humburgerNav.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
+  });
 });
 
-function ftntonav() {
-  if (togmenu.style.display === 'none') {
-    togmenu.style.display = 'block';
-    jtonav.style.color = 'white';
-    jtonav.innerHTML = '<i class="fas fa-times"></i>';
+function displayNav() {
+  if (togmenu.style.display === "none") {
+    togmenu.style.display = "block";
+    humburgerNav.style.color = "white";
+    humburgerNav.innerHTML = '<i class="fas fa-times"></i>';
   } else {
-    togmenu.style.display = 'none';
-    jtonav.style.color = '#6070ff';
-    jtonav.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
+    togmenu.style.display = "none";
+    humburgerNav.style.color = "#6070ff";
+    humburgerNav.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
   }
 }
 
-jtonav.addEventListener('click', ftntonav);
+humburgerNav.addEventListener("click", displayNav);
