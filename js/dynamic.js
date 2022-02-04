895a8dfc0d43649d03307da2a0825c8d1b88b2ca
kano <kanokanombola@gmail.com>
2022-02-05 01:07:35 +0200

    Add JavaScript object for data storage in the browser

--- js/dynamic.js
+++ js/dynamic.js
@@ -11,18 +11,33 @@ menulist.forEach((element) => {
 });
 
 function displayNav() {
-  const menu = document.querySelector('nav > ul');
-  if (menu.style.display == 'block'){
-    menu.style.display = 'none';
+  const menu = document.querySelector("nav > ul");
+  if (menu.style.display == "block") {
+    menu.style.display = "none";
     humburgerNav.style.color = "#6070ff";
     humburgerNav.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
   } else {
-    menu.style.display = 'block';
+    menu.style.display = "block";
     humburgerNav.style.color = "white";
     humburgerNav.innerHTML = '<i class="fas fa-times"></i>';
   }
 
-  menu.classList.toggle('menu');
+  menu.classList.toggle("menu");
 }
 
 humburgerNav.addEventListener("click", displayNav);
+
+//data storage in the browser
+const names = document.getElementById("name").value;
+const email = document.getElementById("mail").value;
+const mess = document.getElementById("message").value;
+const btn = document.getElementById("submitBtn").value;
+
+const User = {
+  name: names,
+  mail: email,
+  message: mess,
+};
+
+btn.addEventListener("click", processData);
+function processData() {}
