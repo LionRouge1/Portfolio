9b4d1adbcb3093ff01e98683b165cad7d8c8a090
kano <kanokanombola@gmail.com>
2022-02-04 17:53:05 +0200

    Add from validation function

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,184 +1,184 @@
 const humburgerNav = document.getElementsByClassName("tonav")[0];
 const menulist = document.querySelectorAll("nav > ul > li");
-const menu = document.querySelector('nav > ul');
-
+const menu = document.querySelector("nav > ul");
 
 menulist.forEach((element) => {
   element.addEventListener("click", () => {
     menu.style.display = "none";
     humburgerNav.style.color = "#6070ff";
     humburgerNav.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
-    menu.classList.toggle('menu');
+    menu.classList.toggle("menu");
   });
 });
 
 function displayNav() {
-  if (menu.style.display == 'block'){
-    menu.style.display = 'none';
+  if (menu.style.display == "block") {
+    menu.style.display = "none";
     humburgerNav.style.color = "#6070ff";
     humburgerNav.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
   } else {
-    menu.style.display = 'block';
+    menu.style.display = "block";
     humburgerNav.style.color = "white";
     humburgerNav.innerHTML = '<i class="fas fa-times"></i>';
   }
 
-  menu.classList.toggle('menu');
+  menu.classList.toggle("menu");
 }
 
 humburgerNav.addEventListener("click", displayNav);
 
-
-// Detail popup windows 
+// Detail popup windows
 
 const projects = [
   {
-    'name' : ['Tonic','CANOPY','Back End Dev','2015'],
-    'description' : 'A daily selection of privately personalized reads; no accounts or sign-ups required.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus voluptates adipisci nobis. Adipisci blanditiis nesciunt hic modi quas quos recusandae cum ab officia iusto non ratione consequatur natus, illum assumenda.',
-    'featured_image' : ['images/Snapshoot.png','images/Dtop_snapshoot2.png'],
-    'technologie' : ['html','css','javaScript'],
-    'link' : ['link-to-live-version','link-to-source-version']
+    name: ["Tonic", "CANOPY", "Back End Dev", "2015"],
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus voluptates adipisci nobis. Adipisci blanditiis nesciunt hic modi quas quos recusandae cum ab officia iusto non ratione consequatur natus, illum assumenda.",
+    featured_image: ["images/Snapshoot.png", "images/Dtop_snapshoot2.png"],
+    technologie: ["html", "css", "javaScript"],
+    link: ["link-to-live-version", "link-to-source-version"],
   },
   {
-    'name' : ['Multi-Post Stories','FACEBOOK','Full Stack Dev','2015'],
-    'description' : 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus voluptates adipisci nobis. Adipisci blanditiis nesciunt hic modi quas quos recusandae cum ab officia iusto non ratione consequatur natus, illum assumenda.',
-    'featured_image' : ['images/Snapshoot1.png','images/Dtop_snapshoot3.png'],
-    'technologie' : ['html','css','Ruby on rails','javaScript'],
-    'link' : ['link-to-live-version','link-to-source-version']
+    name: ["Multi-Post Stories", "FACEBOOK", "Full Stack Dev", "2015"],
+    description:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus voluptates adipisci nobis. Adipisci blanditiis nesciunt hic modi quas quos recusandae cum ab officia iusto non ratione consequatur natus, illum assumenda.",
+    featured_image: ["images/Snapshoot1.png", "images/Dtop_snapshoot3.png"],
+    technologie: ["html", "css", "Ruby on rails", "javaScript"],
+    link: ["link-to-live-version", "link-to-source-version"],
   },
   {
-    'name' : ['Facebook 360','FACEBOOK','Full Stack Dev','2015'],
-    'description' : 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus voluptates adipisci nobis. Adipisci blanditiis nesciunt hic modi quas quos recusandae cum ab officia iusto non ratione consequatur natus, illum assumenda.',
-    'featured_image' : ['images/Snapshoot2.png','images/Dtop_snapshoot.png'],
-    'technologie' : ['html','css','Ruby on rails','javaScript'],
-    'link' : ['https//link-to-live-version','https//link-to-source-version']
+    name: ["Facebook 360", "FACEBOOK", "Full Stack Dev", "2015"],
+    description:
+      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus voluptates adipisci nobis. Adipisci blanditiis nesciunt hic modi quas quos recusandae cum ab officia iusto non ratione consequatur natus, illum assumenda.",
+    featured_image: ["images/Snapshoot2.png", "images/Dtop_snapshoot.png"],
+    technologie: ["html", "css", "Ruby on rails", "javaScript"],
+    link: ["https//link-to-live-version", "https//link-to-source-version"],
   },
   {
-    'name' : ['Uber Navigation','Uber','Lead Developer','2018'],
-    'description' : 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus voluptates adipisci nobis. Adipisci blanditiis nesciunt hic modi quas quos recusandae cum ab officia iusto non ratione consequatur natus, illum assumenda.',
-    'featured_image' : ['images/Snapshoot3.png','images/Dtop_snapshoot1.png'],
-    'technologie' : ['html','css','Ruby on rails','javaScript'],
-    'link' : ['https//link-to-live-version','https//link-to-source-version']
+    name: ["Uber Navigation", "Uber", "Lead Developer", "2018"],
+    description:
+      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.<br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus voluptates adipisci nobis. Adipisci blanditiis nesciunt hic modi quas quos recusandae cum ab officia iusto non ratione consequatur natus, illum assumenda.",
+    featured_image: ["images/Snapshoot3.png", "images/Dtop_snapshoot1.png"],
+    technologie: ["html", "css", "Ruby on rails", "javaScript"],
+    link: ["https//link-to-live-version", "https//link-to-source-version"],
+  },
+];
+
+function WindowPopup(project) {
+  const popup = document.createElement("div");
+  popup.classList.add("popup");
+
+  const popupCont = document.createElement("div");
+  popupCont.classList.add("popupCont");
+
+  const popuptitle = document.createElement("div");
+  popuptitle.classList.add("popuptitle");
+
+  popup.appendChild(popupCont);
+  popupCont.appendChild(popuptitle);
+
+  const htitle = document.createElement("h2"); //variable
+  htitle.textContent = project.name[0];
+  const popclose = document.createElement("i");
+  popclose.setAttribute("class", "fas fa-times");
+
+  popuptitle.appendChild(htitle);
+  popuptitle.appendChild(popclose);
+
+  const frame = document.createElement("div");
+  frame.classList.add("frame");
+  popupCont.appendChild(frame);
+
+  const subTitle = document.createElement("h3"); //variable
+  subTitle.textContent = project.name[1];
+  frame.appendChild(subTitle);
+
+  const dot = document.createElement("div");
+  frame.appendChild(dot);
+
+  const pSub = document.createElement("p"); //variable
+  pSub.textContent = project.name[2];
+  frame.appendChild(pSub);
+
+  const dot1 = document.createElement("div");
+  frame.appendChild(dot1);
+
+  const pYr = document.createElement("p"); //variable
+  pYr.textContent = project.name[3];
+  frame.appendChild(pYr);
+
+  const mobileImg = document.createElement("img");
+  mobileImg.setAttribute("src", project.featured_image[0]); //variable
+  mobileImg.setAttribute("alt", "A facebook card for mobile"); //variable
+  mobileImg.classList.add("phone");
+  popupCont.appendChild(mobileImg);
+
+  const DestImg = document.createElement("img");
+  DestImg.setAttribute("src", project.featured_image[1]); //variable
+  DestImg.setAttribute("alt", "A facebook card for mobile"); //variable
+  DestImg.classList.add("Dtop");
+  popupCont.appendChild(DestImg);
+
+  const popupBody = document.createElement("div");
+  popupBody.classList.add("popupbody");
+  popupCont.appendChild(popupBody);
+
+  const text = document.createElement("p"); //variable
+  text.textContent = project.description;
+  text.classList.add("text");
+  popupBody.appendChild(text);
+
+  const cardre = document.createElement("div");
+  popupBody.appendChild(cardre);
+
+  const lang = document.createElement("ul"); //variable array
+
+  for (let i = 0; i < project.technologie.length; i++) {
+    const list = document.createElement("li");
+    list.innerHTML = project.technologie[i];
+    lang.appendChild(list);
+    if (project.technologie[i] == "Ruby on rails") {
+      list.classList.add("ruby");
+    }
   }
-]
 
+  lang.classList.add("lang");
+  cardre.appendChild(lang);
 
-function WindowPopup(project){
-const popup = document.createElement('div');
-popup.classList.add('popup');
+  const btnbox = document.createElement("div");
+  btnbox.classList.add("btnbox");
+  cardre.appendChild(btnbox);
 
-const popupCont = document.createElement('div');
-popupCont.classList.add('popupCont');
+  const btnLive = document.createElement("a");
+  btnLive.setAttribute("type", "button");
+  btnLive.href = project.link[0];
+  btnLive.classList.add("btn");
+  btnbox.appendChild(btnLive);
+  btnLive.innerHTML = 'See live <i class="fas fa-external-link-alt"></i>';
 
-const popuptitle = document.createElement('div');
-popuptitle.classList.add('popuptitle');
+  const btnSource = document.createElement("a");
+  btnSource.setAttribute("tupe", "button");
+  btnSource.href = project.link[1];
+  btnSource.classList.add("btn");
+  btnbox.appendChild(btnSource);
+  btnSource.innerHTML = 'See source <i class="fab fa-github"></i>';
 
-popup.appendChild(popupCont);
-popupCont.appendChild(popuptitle);
+  const pr = document.querySelector(".snap_container");
+  const bt = pr.firstChild;
+  pr.insertBefore(popup, bt);
 
-const htitle = document.createElement('h2');//variable
-htitle.textContent = project.name[0]
-const popclose = document.createElement('i');
-popclose.setAttribute('class', 'fas fa-times');
-
-
-popuptitle.appendChild(htitle);
-popuptitle.appendChild(popclose);
-
-const frame = document.createElement('div');
-frame.classList.add('frame');
-popupCont.appendChild(frame);
-
-const subTitle = document.createElement('h3');//variable
-subTitle.textContent = project.name[1];
-frame.appendChild(subTitle);
-
-const dot = document.createElement('div');
-frame.appendChild(dot);
-
-const pSub = document.createElement('p');//variable
-pSub.textContent = project.name[2];
-frame.appendChild(pSub);
-
-const dot1 = document.createElement('div');
-frame.appendChild(dot1);
-
-const pYr = document.createElement('p');//variable
-pYr.textContent = project.name[3]
-frame.appendChild(pYr);
-
-const mobileImg = document.createElement('img');
-mobileImg.setAttribute('src', project.featured_image[0]);//variable
-mobileImg.setAttribute('alt', 'A facebook card for mobile');//variable
-mobileImg.classList.add('phone');
-popupCont.appendChild(mobileImg);
-
-const DestImg = document.createElement('img');
-DestImg.setAttribute('src', project.featured_image[1]);//variable
-DestImg.setAttribute('alt', 'A facebook card for mobile');//variable
-DestImg.classList.add('Dtop');
-popupCont.appendChild(DestImg);
-
-const popupBody = document.createElement('div');
-popupBody.classList.add('popupbody');
-popupCont.appendChild(popupBody);
-
-const text = document.createElement('p');//variable
-text.textContent = project.description;
-text.classList.add('text');
-popupBody.appendChild(text);
-
-const cardre = document.createElement('div');
-popupBody.appendChild(cardre);
-
-const lang = document.createElement('ul');//variable array
-
-for(let i=0; i < project.technologie.length; i++){
-  const list = document.createElement('li');
-  list.innerHTML = project.technologie[i];
-  lang.appendChild(list);
-  if(project.technologie[i] == 'Ruby on rails'){
-    list.classList.add('ruby');
-  }
+  popclose.addEventListener("click", () => {
+    pr.removeChild(popup);
+  });
 }
 
-lang.classList.add('lang');
-cardre.appendChild(lang);
+const pro = document.querySelectorAll(".left_block > button");
 
-const btnbox = document.createElement('div');
-btnbox.classList.add('btnbox');
-cardre.appendChild(btnbox);
-
-const btnLive = document.createElement('a');
-btnLive.setAttribute('type', 'button');
-btnLive.href = project.link[0];
-btnLive.classList.add('btn');
-btnbox.appendChild(btnLive);
-btnLive.innerHTML = 'See live <i class="fas fa-external-link-alt"></i>';
-
-const btnSource = document.createElement('a');
-btnSource.setAttribute('tupe', 'button');
-btnSource.href = project.link[1];
-btnSource.classList.add('btn');
-btnbox.appendChild(btnSource);
-btnSource.innerHTML = 'See source <i class="fab fa-github"></i>';
-
-const pr = document.querySelector('.snap_container');
-const bt = pr.firstChild;
-pr.insertBefore(popup, bt);
-
-popclose.addEventListener('click',()=> {
-  pr.removeChild(popup);
-});
+for (let i = 0; i < pro.length; i++) {
+  pro[i].addEventListener("click", WindowPopup.bind(null, projects[i]), false);
 }
 
-const pro = document.querySelectorAll('.left_block > button');
-
-for (let i = 0; i < pro.length; i++){
-  pro[i].addEventListener('click', WindowPopup.bind(null, projects[i]),false);
-}
-
-const sectionCard = document.querySelector('.snap_container');
+const sectionCard = document.querySelector(".snap_container");
 
 const cardArticle = `
 <article>
@@ -217,13 +217,25 @@ const cardArticle = `
         </article>
 
 
-`
-function makeList(arr){
-  return arr.map((itm)=>{
-return `<li>${itm}</li>`
-  })
+`;
+function makeList(arr) {
+  return arr.map((itm) => {
+    return `<li>${itm}</li>`;
+  });
 }
 
+sectionCard.insertAdjacentHTML("afterbegin", cardArticle);
 
+// form validation
+const submitBtn = document.getElementById("submitBtn");
+console.log(submitBtn);
+function validator(e) {
+  const formValue = document.getElementById("email").value;
+  if (formValue === formValue.toLowerCase()) {
+    console.log(formValue,e);
+  } else {
+    e.preventDefault();
+  }
+}
 
-sectionCard.insertAdjacentHTML('afterbegin',cardArticle);
+submitBtn.addEventListener("click", validator);
