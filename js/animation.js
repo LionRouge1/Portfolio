33dc3d3564384d3162e9f40e08c9398baabd5515
Masoudi Parrot <masoudi@localhost.localdomain>
2022-06-05 22:06:24 -0400

    add typing animation

--- js/animation.js
+++ js/animation.js
@@ -1,9 +1,45 @@
 const left_blocks = document.querySelectorAll('article > .left_block');
 const snapshoots = document.querySelectorAll('article > .snapshoot');
+const txt = document.querySelector('.aboutText').textContent;
+document.querySelector('.aboutText').textContent = '';
+console.log(txt);
 
-observerConfig = {
+let y = 0;
+const typeWriterh1 = () => {
+  let speed = 200;
+  if (y < txt.length) {
+    document.querySelector('.aboutText').textContent += txt.charAt(y);
+    y++;
+    setTimeout(typeWriterh1, speed);
+  }
+}
+
+function getView(a,b = document.querySelector('.aboutText')){
+  let observerConfg = {
+    root: document,
+    rootMargin: '-200px',
+    threshold: 1
+  }
+
+  const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        a();
+      }
+    });
+  });
+  
+  observer.observe(b, observerConfg);
+}
+
+getView(typeWriterh1);
+
+let observerConfig = {
   root: document,
-  rootMargin: '-50px',
+  rootMargin: '0px',
   threshold: 1
 }
 
